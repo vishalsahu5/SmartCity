a481ea525302aa8ce8a6d28b7e5b66e73ff40a2f
Sarthak Sahu <vishalsahu5@gmail.com>
2018-05-27 18:02:27 +0530

    separate icons for dustbin and parking

--- static/js/overall_js.js
+++ static/js/overall_js.js
@@ -5,11 +5,12 @@ $(document).ready(function () {
         url: 'http://127.0.0.1:8000/dustbin/dustbins/?format=json',
         success: function (data) {
             var dustbin_points = [];
+            var type = "DUSTBIN";
             console.log('success', data);
             for (var i = 0; i < data.length; i++) {
                 dustbin_points.push(new google.maps.LatLng(data[i]['latitude'], data[i]['longitude']));
             }
-            addMarkers(dustbin_points);
+            addMarkers(dustbin_points, type);
         }
     });
     $.ajax({
@@ -17,27 +18,39 @@ $(document).ready(function () {
         url: 'http://127.0.0.1:8000/parking/parking_lots/?format=json',
         success: function (data) {
             var parking_points = [];
+            var type = "PARKING";
             console.log('success', data);
             for (var i = 0; i < data.length; i++) {
                 parking_points.push(new google.maps.LatLng(data[i]['latitude'], data[i]['longitude']));
             }
-            addMarkers(parking_points);
+            addMarkers(parking_points, type);
         }
     });
 });
 
-function addMarkers(points) {
+function addMarkers(points, type) {
     var marker, i;
     if (points) {
         var size = points.length;
-        for (i = 0; i < size; i++) {
-            marker = new google.maps.Marker({
-                position: points[i],
-                map: map,
-                icon: 'https://maps.google.com/mapfiles/kml/shapes/parking_lot_maps.png'
-            });
-            console.log('Success !');
+        if (type === "DUSTBIN") {
+            for (i = 0; i < size; i++) {
+                marker = new google.maps.Marker({
+                    position: points[i],
+                    map: map,
+                });
+                console.log('Success !');
+            }
+        } else if (type === "PARKING") {
+            for (i = 0; i < size; i++) {
+                marker = new google.maps.Marker({
+                    position: points[i],
+                    map: map,
+                    icon: 'https://maps.google.com/mapfiles/kml/shapes/parking_lot_maps.png'
+                });
+                console.log('Success !');
+            }
         }
+
     }
 }
 
