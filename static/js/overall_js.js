4d2d6a24533c321644b23de9acb6d0c3098f4ed8
Sarthak Sahu <vishalsahu5@gmail.com>
2018-05-27 17:58:22 +0530

    js file for showing map changed.

--- static/js/overall_js.js
+++ static/js/overall_js.js
@@ -1,31 +1,43 @@
-var points = [];
 var map = null;
 $(document).ready(function () {
     $.ajax({
         type: 'GET',
         url: 'http://127.0.0.1:8000/dustbin/dustbins/?format=json',
         success: function (data) {
+            var dustbin_points = [];
             console.log('success', data);
             for (var i = 0; i < data.length; i++) {
-                points.push(new google.maps.LatLng(data[i]['latitude'], data[i]['longitude']));
+                dustbin_points.push(new google.maps.LatLng(data[i]['latitude'], data[i]['longitude']));
             }
-            addMarkers();
+            addMarkers(dustbin_points);
+        }
+    });
+    $.ajax({
+        type: 'GET',
+        url: 'http://127.0.0.1:8000/parking/parking_lots/?format=json',
+        success: function (data) {
+            var parking_points = [];
+            console.log('success', data);
+            for (var i = 0; i < data.length; i++) {
+                parking_points.push(new google.maps.LatLng(data[i]['latitude'], data[i]['longitude']));
+            }
+            addMarkers(parking_points);
         }
     });
 });
 
-function addMarkers() {
+function addMarkers(points) {
     var marker, i;
-    var size = points.length;
-    var default_location = {lat: 0.0, lng: 0.0};
-    for (i = 0; i < size; i++) {
-        marker = new google.maps.Marker({
-            position: points[i],
-            map: map,
-            title: 'Dustbin',
-            icon: 'https://maps.google.com/mapfiles/kml/shapes/parking_lot_maps.png'
-        });
-        console.log('Success !');
+    if (points) {
+        var size = points.length;
+        for (i = 0; i < size; i++) {
+            marker = new google.maps.Marker({
+                position: points[i],
+                map: map,
+                icon: 'https://maps.google.com/mapfiles/kml/shapes/parking_lot_maps.png'
+            });
+            console.log('Success !');
+        }
     }
 }
 
